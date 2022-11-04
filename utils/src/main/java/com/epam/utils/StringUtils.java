6414cd83bdcd94cf21b687daceb5d0591c35022d
Petya4000 <pt.bartosh54321@gmail.com>
2022-11-04 13:44:18 +0300

    commit 1

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null) return false;
+        return !str.contains("-") && !str.trim().equals("") && !str.trim().equals("0");
     }
 }
