77ca7ec30f2c7eddf58a68ff41baf860900d8aa2
Petya4000 <pt.bartosh54321@gmail.com>
2022-11-04 15:55:02 +0300

    commit 6 (trying pass last test)

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if (args == null) return false;
+        if (args.isEmpty()) return false;
        for (String str : args)
            if (!StringUtils.isPositiveNumber(str)) return false;
            return true;
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,6 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         if (str == null) return false;
-        return !str.contains("-") && !str.trim().equals("") && !str.trim().equals("0");
+        return !str.contains("-") && !str.trim().isEmpty() && !str.trim().equals("0");
     }
 }
